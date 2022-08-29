0c3670f0e53640cbd698b758338130e2d6257b88
Sanjay Sodani <Sanjay.Sodani@thesmartq.com>
2022-08-29 20:05:24 +0530

    ohho

--- src/App.js
+++ src/App.js
@@ -1,72 +1,140 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
-  const handleButtonClick = () => {
-    const data = [
-      {
-        cmd: "printcenter",
-        parameters: ["**** ORDER MODIFIED ****"],
-        fontsize: { width: 2, height: 2 }
-      },
-      {
-        cmd: "printcenter",
-        parameters: ["Please review this order"],
-        fontsize: { width: 2, height: 2 }
-      },
-      { cmd: "printcenter", parameters: ["", "", "KOT"] },
-      { cmd: "printNothing", parameters: [] },
-      {
-        cmd: "printcenter",
-        parameters: ["31"],
-        fontsize: { width: 2, height: 2 }
-      },
-      { cmd: "printcenter", parameters: ["Breakfast - T&T"] },
-      { cmd: "printleft", parameters: ["Date: 2022-08-29 13:48:27"] },
-      { cmd: "printNothing", parameters: [] },
-      { cmd: "printitemdetails", parameters: ["Product", "Qty", "Total"] },
-      { cmd: "printdivider", parameters: [] },
-      {
-        cmd: "printitemdetails",
-        parameters: ["111 Apfelkuchen", "1", "£2.00"]
-      },
-      {
-        cmd: "printitemdetails",
-        parameters: ["Pasta & Vegetable Salad", "1", "£1.25"]
-      },
-      { cmd: "printitemdetails", parameters: ["Slaw", "1", "£3.50"] },
-      { cmd: "printleftright", parameters: ["TotalQty: 3", "Amt: £6.75"] },
-      {
-        cmd: "printcenter",
-        parameters: [" ", "***Cancelled Item(s)***"],
-        fontsize: { width: 2, height: 2 }
-      },
-      { cmd: "printitemdetails", parameters: ["Product", "Qty", "Total"] },
-      { cmd: "printdivider", parameters: [] },
-      {
-        cmd: "printleftright",
-        parameters: ["Green Seasonal Salad", "1", "£1.25"]
-      },
-      { cmd: "printleftright", parameters: ["1 Caesar Salad", "1", "£0.00"] },
-      { cmd: "printdivider", parameters: [] },
-      { cmd: "printleftright", parameters: ["TotalQty:2", "Amt:£1.25"] },
-      { cmd: "printleft", parameters: ["Payment mode:Online: £6.75"] },
-      {
-        cmd: "printleft",
-        parameters: ["Ref number: TE1-0000031-760Z22829134848260"]
-      },
-      { cmd: "printcenter", parameters: ["****"] },
-      { cmd: "printleft", parameters: ["Printed at :2022-08-29 13:49:43"] },
-      { cmd: "printleft", parameters: ["Manually Printed"] },
-      { cmd: "cutpage", parameters: [] }
-    ];
-    let dataString = JSON.stringify(data);
-    console.log(dataString);
-    window.webprint.printText(dataString);
+  const [fontSize, setFontSize] = useState("1");
+  const [command, setCommand] = useState("select");
+  const [text, setText] = useState({
+    text: "",
+    isDisabled: true
+  });
+  const [printText, setPrintText] = useState([]);
+
+  const handleCommandChanged = com => {
+    setCommand(com);
   };
+
+  const handleTextChanged = inputText => {
+    setText({ text: inputText });
+  };
+
+  const handleAddCommand = () => {
+    if (
+      command !== "select" &&
+      command !== "printdivider" &&
+      command !== "printNothing" &&
+      command !== "select"
+    ) {
+      const printArray = printText;
+      printArray.push({
+        cmd: command,
+        parameters: text.text.split("$"),
+        fontsize: { width: fontSize, height: fontSize }
+      });
+      setPrintText(printArray);
+    } else if (command === "select") {
+      window.alert("Please choose a command!");
+    } else {
+      const printArray = printText;
+      printArray.push({
+        cmd: command,
+        parameters: []
+      });
+      setPrintText(printArray);
+    }
+  };
+
+  const handlePrint = () => {
+    if (printText.length !== 0) {
+      const printArray = printText;
+      printArray.push({
+        cmd: "cutpage",
+        parameters: []
+      });
+      setPrintText(printArray);
+      let dataString = JSON.stringify(printText);
+      console.log(dataString);
+      window.webprint.printText(dataString);
+      setCommand("select");
+      setPrintText([]);
+      setText("");
+    }
+  };
+
+  // const handleButtonClick = () => {
+  //   const data = [
+  //     {
+  //       cmd: "printcenter",
+  //       parameters: ["**** ORDER MODIFIED ****"],
+  //       fontsize: { width: 2, height: 2 }
+  //     }
+  //   ];
+  //   let dataString = JSON.stringify(data);
+  //   console.log(dataString);
+  //   window.webprint.printText(dataString);
+  // };
+
   return (
     <div className="App">
-      <h2>Test_aa</h2>
-      <button onClick={handleButtonClick}>Print</button>
+      <h2>Test the Printer</h2>
+      <ol>
+        <li>Choose a command</li>
+        <li>Change the font if required</li>
+        <li>
+          Type the printing text if required and click <strong>Add</strong>
+        </li>
+        <li>
+          Click on <strong>Print</strong>
+        </li>
+      </ol>
+      <ul>
+        {printText.map((item, index) => {
+          return <li key={index}>{item.cmd}</li>;
+        })}
+      </ul>
+      <label htmlFor="font">Choose font size: </label>
+      <select
+        id="font"
+        name="font"
+        onChange={e => {
+          setFontSize(e.target.value);
+        }}
+        value={fontSize}
+      >
+        <option value="1">1</option>
+        <option value="2">2</option>
+      </select>
+      <br /> <br />
+      <label htmlFor="command">Choose a command: </label>
+      <select
+        id="command"
+        name="command"
+        onChange={e => {
+          handleCommandChanged(e.target.value);
+        }}
+        value={command}
+      >
+        <option value="select">Select</option>
+        <option value="printcenter">Print Center</option>
+        <option value="printNothing">Print Nothing</option>
+        <option value="printleft">Print Left</option>
+        <option value="printitemdetails">Print Item Details</option>
+        <option value="printdivider">Print Divider</option>
+        <option value="printright">Print Right</option>
+      </select>
+      <br /> <br />
+      <small>Use '$' to seprate the string</small> <br />
+      <input
+        type="text"
+        placeholder="Type something"
+        value={text.text}
+        onChange={e => handleTextChanged(e.target.value)}
+      />
+      <button onClick={handleAddCommand}>+ Add</button>
+      <br /> <br />
+      <button className="buttonPrint" onClick={handlePrint}>
+        Print
+      </button>
     </div>
   );
 }

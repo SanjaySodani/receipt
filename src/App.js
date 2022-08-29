1b4df8b3ce1d50609a1ae54178af800938fe8738
Sanjay Sodani <Sanjay.Sodani@thesmartq.com>
2022-08-29 13:29:47 +0530

    React now

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import "./App.css";
+
+function App() {
+  const handleButtonClick = () => {
+    const data = [
+      {
+        cmd: "printcenter",
+        parameters: ["***Cancelled Item(s)***"],
+        fontsize: {
+          width: 2,
+          height: 2
+        }
+      },
+      {
+        cmd: "printcenter",
+        parameters: ["*** Order Modified ***"],
+        fontsize: {
+          width: 2,
+          height: 2
+        }
+      },
+      {
+        cmd: "printcenter",
+        parameters: ["Please review this order"],
+        fontsize: {
+          width: 2,
+          height: 2
+        }
+      }
+    ];
+    let dataString = JSON.stringify(data);
+    console.log(dataString);
+    window.webprint.printText(dataString);
+  };
+  return (
+    <div className="App">
+      <h2>Test_aa</h2>
+      <button onClick={handleButtonClick}>Print</button>
+    </div>
+  );
+}
+
+export default App;
